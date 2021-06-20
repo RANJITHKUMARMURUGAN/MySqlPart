a224ab9ea4d72e19b68f52806cbebb1d7610708a
unknown <ranjithm1193@gmail.com>
2021-06-20 15:26:10 +0530

    [Ranjith] added EmployeePayrollFileIOService.java

---
+++ EmployeePayrollFileIOService.java
@@ -0,0 +1,21 @@
+package com.jdbc;
+
+import java.util.List;
+
+import com.jdbc.EmployeePayrollService.IOService;
+
+public class EmployeePayrollFileIOService {
+
+	public void printData() {
+		
+	}
+
+	public void countEntries() {
+		
+	}
+
+	public void writeData(List<EmployeePayrollData> employeePayrollList, IOService ioService) {
+		
+	}
+
+}
